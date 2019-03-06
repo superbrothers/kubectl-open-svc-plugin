7f4de108367626ea6236676fe71a71afdf55dc4c
Kazuki Suda <kazuki.suda@gmail.com>
2019-03-06 08:29:42 +0000

    Don't use the latest version of krew

--- hack/install-krew.sh
+++ hack/install-krew.sh
@@ -2,7 +2,7 @@
 
 set -e -o pipefail
 
-KREW_VERSION="$(curl --silent -L https://api.github.com/repos/GoogleContainerTools/krew/releases | jq -r '[.[] | select(.prerelease==false)][0] | .tag_name')"
+KREW_VERSION="v0.2.1"
 (
   set -x; cd "$(mktemp -d)" &&
   curl -fsSLO "https://storage.googleapis.com/krew/${KREW_VERSION}/krew.{tar.gz,yaml}" &&
