ece828045b7d3387ebab8b8e6506b2b62bd1abe2
Kazuki Suda <230185+superbrothers@users.noreply.github.com>
2019-12-17 22:03:27 +0900

    Upgrade vendors to Kubernetes 1.17 (#28)

--- hack/update-dependencies.sh
+++ hack/update-dependencies.sh
@@ -3,13 +3,14 @@
 set -e -o pipefail; [[ -n "$DEBUG" ]] && set -x
 
 dependencies=(
-  "k8s.io/client-go@kubernetes-1.16.0" \
-  "k8s.io/cli-runtime@kubernetes-1.16.0" \
-  "k8s.io/apimachinery@kubernetes-1.16.0" \
-  "k8s.io/kubectl@kubernetes-1.16.0" \
-  "k8s.io/klog@v0.3.3" \
+  "k8s.io/client-go@kubernetes-1.17.0" \
+  "k8s.io/cli-runtime@kubernetes-1.17.0" \
+  "k8s.io/apimachinery@kubernetes-1.17.0" \
+  "k8s.io/kubectl@kubernetes-1.17.0" \
+  "k8s.io/klog@v1.0.0" \
   "github.com/spf13/cobra@v0.0.5" \
   "github.com/pkg/browser@master" \
 )
 
 GO111MODULE=on go get "${dependencies[@]}"
+GO111MODULE=on go mod tidy
