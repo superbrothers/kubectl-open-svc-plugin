17c2f248d5a088fd2da5fca2b0f87b4281a711b4
Kazuki Suda <kazuki.suda@gmail.com>
2021-05-08 17:20:33 +0900

    Upgrade klog to v2

--- cmd/kubectl-open_svc.go
+++ cmd/kubectl-open_svc.go
@@ -1,23 +1,19 @@
 package main
 
 import (
-	"flag"
 	"os"
 
 	"github.com/spf13/pflag"
 	"github.com/superbrothers/kubectl-open-svc-plugin/pkg/cmd"
 
 	"k8s.io/cli-runtime/pkg/genericclioptions"
-	"k8s.io/klog"
+	"k8s.io/klog/v2"
 )
 
-func init() {
-	// Initialize glog flags
-	klog.InitFlags(flag.CommandLine)
-	_ = flag.CommandLine.Set("logtostderr", "true")
-}
-
 func main() {
+	klog.InitFlags(nil)
+	defer klog.Flush()
+
 	flags := pflag.NewFlagSet("kubectl-open-svc", pflag.ExitOnError)
 	pflag.CommandLine = flags
 
